ea6524ff687ae9c0e201b9b2484cd69e9d0ceee8
vanshika gupta <1shekadeveloperdesigner@gmail.com>
2026-01-08 13:23:43 +0530

    learn how to used maximum number in loop

---
+++ day-4/max_in_loop.py
@@ -0,0 +1,9 @@
+student_score=[10,25,26,28,56,59,63,48,56,57,48,5,59,53,36,9,98]
+
+max_score = 0
+for score in student_score:
+    if score > max_score:
+        max_score = score
+print(max_score," max score")
+
+
